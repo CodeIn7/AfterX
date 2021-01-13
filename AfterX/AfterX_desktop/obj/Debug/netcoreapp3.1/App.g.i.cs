8d1c12b31f22f3ef324a705be9d8a53d01cae9d7
Zdravko <pandzic01@gmail.hr>
2021-01-13 22:00:30 +0100

    v3

--- AfterX/AfterX_desktop/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ AfterX/AfterX_desktop/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace AfterX_desktop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AfterX_desktop {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public static void Main() {
             AfterX_desktop.App app = new AfterX_desktop.App();
             app.InitializeComponent();
---
+++ DB/insert_data.sql
@@ -0,0 +1,13 @@
+
+"drinkType"
+
+INSERT INTO public."drinkType"(name) VALUES ('WINE');
+INSERT INTO public."drinkType"(name) VALUES ('BEER');
+INSERT INTO public."drinkType"(name) VALUES ('WHISKY');
+INSERT INTO public."drinkType"(name) VALUES ('VODKA');
+INSERT INTO public."drinkType"(name) VALUES ('GIN');
+INSERT INTO public."drinkType"(name) VALUES ('TEQUILA');
+INSERT INTO public."drinkType"(name) VALUES ('COGNAC');
+INSERT INTO public."drinkType"(name) VALUES ('SODA');
+INSERT INTO public."drinkType"(name) VALUES ('ENERGY');
+INSERT INTO public."drinkType"(name) VALUES ('SOFT');
